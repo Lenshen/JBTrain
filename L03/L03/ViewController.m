69c928ed8dac38fbd26ba971ce58a85ed8aa01d8
MrMessy77 <mrmessy@billys-MacBook-Pro.local>
2015-05-18 11:51:10 +0800

    Added label

--- L03/L03/ViewController.m
+++ L03/L03/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "BOUIView.h"
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *boScroolView;
 
@@ -17,7 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    BOUIView *boLabel = [[BOUIView alloc] initWithFrame:CGRectMake(10, 10, 160, 160)];
+//    [self.boScroolView addSubview:boLabel];
+//    boLabel.backgroundColor = [UIColor redColor];
+//    
+////    CGRect boScrollViewBounds = self.boScroolView.bounds;
+////    CGRect boLabelFrame = boLabel.frame;
+////    
+////    CGFloat boLabelX = (boScrollViewBounds.size.width - boLabelFrame.size.width)/2.0;
+////    CGFloat boLabelY = (boScrollViewBounds.size.height - boLabelFrame.size.height)/2.0;
+////    
+////    CGRect newFrame = CGRectMake(boLabelX, boLabelY, boLabelFrame.size.width, boLabelFrame.size.height);
+////    
+////    boLabel.frame = newFrame;
+//    
+//    UILabel *numberLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+//    numberLabel.text = @"1";
+//    
+//    CGRect boLabelBounds = boLabel.bounds;
+//    CGRect numberLabelFrame = numberLabel.frame;
+//    
+//    
+//    CGFloat numberLabelX = (boLabelBounds.size.width - numberLabelFrame.size.width)/2.0;
+//    CGFloat numberLabelY = (boLabelBounds.size.height - numberLabelFrame.size.height)/2.0;
+//    
+//    CGRect newNumberFrame = CGRectMake(numberLabelX, numberLabelY, numberLabelFrame.size.width, numberLabelFrame.size.height);
+//    numberLabel.frame = newNumberFrame;
+//    
+//    [boLabel addSubview:numberLabel];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
